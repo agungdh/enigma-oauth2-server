2c77e53f5836921097b2615ec762817947b94d8c
AgungDH <agungdh@live.com>
2024-10-31 11:43:59 +0700

    return user role

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    $user = $request->user();
+    $user->roles;
+    $user->role = $user->roles->first();
+    $user->role_name = $user->role->name;
+
+    return $user;
 })->middleware('auth:api');
